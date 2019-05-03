a3b98d6e298529b1b13072542490fcedfbbc5668
Sergio <sergiohidalgocaceres@gmail.com>
2019-05-02 21:57:46 -0500

    Clase02

--- Angular/Clase02/appInputOutput/src/app/app.component.ts
+++ Angular/Clase02/appInputOutput/src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  alumnos: Array<string> = ["Karla", "Juan", "Alfonso", "Viviana", "Pedro", "Inés", "Alfonsina"]
+
+  usuarios: Array<object> = [
+    { username: "usuario 1", area: "area 1" },
+    { username: "usuario 2", area: "area 2" },
+    { username: "usuario 3", area: "area 3" }
+  ]
+
+
   title = 'appInputOutput';
 }
